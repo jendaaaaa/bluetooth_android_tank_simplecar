20f939d1081880816ef63d9225bda10f59d4bb4a
jendaaaaa <143932605+jendaaaaa@users.noreply.github.com>
2023-11-26 11:24:46 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,26 @@
-OMG_Tank.Tank_Init(Tank_version.V3);
-OMG_Tank.Stop_Motors();
-let Data = "";
-bluetooth.startUartService();
 bluetooth.onBluetoothConnected(function () {
-    basic.showIcon(IconNames.Happy);
-});
+    basic.showIcon(IconNames.Happy)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    basic.showIcon(IconNames.No)
+})
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Hash), function () {
-    Data = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash));
-});
+    Data = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Hash))
+})
+let Data = ""
+OMG_Tank.Tank_Init(Tank_version.V3)
+OMG_Tank.Stop_Motors()
+bluetooth.startUartService()
 basic.forever(function () {
-	if (Data === "f"){
-        OMG_Tank.Set_Speed(100, 100);
-    } else if (Data === "b"){
-        OMG_Tank.Set_Speed(-100, -100);
-    } else if (Data === "l"){
-        OMG_Tank.Set_Speed(-100, 100);
-    } else if (Data === "r"){
-        OMG_Tank.Set_Speed(100, -100);
+    if (Data == "f") {
+        OMG_Tank.Set_Speed(100, 100)
+    } else if (Data == "b") {
+        OMG_Tank.Set_Speed(-100, -100)
+    } else if (Data == "l") {
+        OMG_Tank.Set_Speed(-100, 100)
+    } else if (Data == "r") {
+        OMG_Tank.Set_Speed(100, -100)
     } else {
-        OMG_Tank.Stop_Motors();
+        OMG_Tank.Stop_Motors()
     }
-});
+})
